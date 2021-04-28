6f61f4126d4f19bf6fd7c27e2bcc9ab19c0d14b3
Shauna Saunders <shaunacsaunders@outlook.com>
2021-04-28 20:24:07 +0000

    Final commit

--- calculator.py
+++ calculator.py
@@ -27,3 +27,31 @@ while True:
 
     # name number variable
     num1 = tokens[1]
+    num2 = tokens[2]
+
+    #calling operator functions
+    if operator == "+":
+        result = add(float(num1), float(num2))
+
+    elif operator == "-":
+        result = subtract(float(num1), float(num2))
+
+    elif operator == "*":
+        result = multiply(float(num1), float(num2))
+
+    elif operator == "/":
+        result = divide(float(num1), float(num2))
+
+    elif operator == "square":
+        result = square(float(num1))
+    
+    elif operator == "cube":
+        result = cube(float(num1))
+    
+    elif operator == "pow":
+        result = power(float(num1), float(num2))
+
+    elif operator == "mod":
+        result = mod(float(num1), float(num2))    
+
+    print(result)    
